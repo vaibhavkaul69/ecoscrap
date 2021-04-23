1b9abdddd5f2cd756c67a81afde0aa6673ff455c
vaibhavkaul69 <vaibhav.kaul2@gmail.com>
2021-04-24 00:16:37 +0530

    Homepage html css done

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -11,3 +11,37 @@ function openCloseSideNavbar() {
 		sideNavbar.classList.add("inactive-close-sidebar");
 	}
 }
+
+(function () {
+	var swiper = new Swiper(".swiper-container", {
+		slidesPerView: 3,
+		spaceBetween: 10,
+		slidesPerGroup: 1,
+		loop: true,
+		loopFillGroupWithBlank: false,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		// Responsive breakpoints
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 1,
+			},
+			// when window width is >= 700px
+			700: {
+				slidesPerView: 2,
+			},
+			// when window width is >= 1000px
+			1000: {
+				slidesPerView: 3,
+			},
+		},
+	});
+	console.log(swiper);
+})();
